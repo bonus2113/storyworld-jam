9cbfa70be8f683a8780da5c433635ff52e9ba557
Remy Stuurwold <remystuurwold+sourcetree@gmail.com>
2014-07-04 21:39:15 +0100

    symbols

---
+++ unity-wizarding/Assets/Scripts/Ritual/Symbol.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Symbol : MonoBehaviour {
+
+    [SerializeField]
+    private int SymbolID = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity-wizarding/Assets/Scripts/Ritual/SymbolManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SymbolManager : MonoBehaviour {
+
+    private int m_NumSymbols = 0;
+
+    [SerializeField]
+    private Vector2 m_SymbolTarget = new Vector2(0, 0);
+
+    [SerializeField]
+    private List<Symbol> m_SymbolList = null;
+
+	// Use this for initialization
+	void Start () {
+
+        this.m_NumSymbols = this.m_SymbolList.Count;
+        if (this.m_NumSymbols == 0)
+        {
+            Debug.LogWarning("No symbols in list");
+            Destroy(this);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void InitialiseSymbols()
+    {
+        const int SymbolYOffset = 10;
+
+        for(int i = 0; i < this.m_NumSymbols; i++)
+        {
+
+        }
+    }
+}
