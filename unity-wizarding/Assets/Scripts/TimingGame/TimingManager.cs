81584cc526c2862cd70c319fefc819e6fae78123
Dario Seyb <dario.seyb@gmail.com>
2014-07-04 22:10:00 +0200

    -added timeline basics

---
+++ unity-wizarding/Assets/Scripts/TimingGame/TimingManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimingManager : MonoBehaviour
+{
+    [SerializeField]
+    private TimeLine[] timeLines;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
