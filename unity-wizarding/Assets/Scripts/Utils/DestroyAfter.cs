7977b397072c6a5fc5347a88930811acf140c78f
Dario Seyb <dario.seyb@gmail.com>
2014-07-06 13:29:08 +0100

    -candle effect

---
+++ unity-wizarding/Assets/Scripts/Utils/DestroyAfter.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using System.Collections;
+
+public class DestroyAfter : MonoBehaviour
+{
+    public float DestroyTime = 0;
+
+    private void Start()
+    {
+        Destroy(gameObject, DestroyTime);
+    }
+}
