14d2fe633ae7312773dd31644391de1cf258a4f7
Dario Seyb <dario.seyb@gmail.com>
2014-07-06 11:43:20 +0100

    -added particle effects

---
+++ unity-wizarding/Assets/ParticleSortingOrder.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Security;
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ParticleSortingOrder : MonoBehaviour
+{
+    public int SortingOrder;
+    public string Layer;
+
+    private ParticleSystem particles;
+	// Use this for initialization
+	void Start ()
+	{
+	    particles = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (particles == null)
+	        particles = GetComponent<ParticleSystem>();
+
+	    if (particles != null)
+	    {
+            particles.renderer.sortingOrder = SortingOrder;
+            particles.renderer.sortingLayerName = Layer;
+	    }
+	}
+}
--- unity-wizarding/Assets/Scripts/Effects/Flicker.cs
+++ unity-wizarding/Assets/Scripts/Effects/Flicker.cs
@@ -14,7 +14,7 @@ public class Flicker : MonoBehaviour
 	
 	void Update ()
 	{
-	    var val = Mathf.Sin(Time.timeSinceLevelLoad)*Mathf.Cos(Time.timeSinceLevelLoad*0.4f + 2)*0.6f + 0.4f;
+	    var val = Mathf.Sin(Time.timeSinceLevelLoad)*Mathf.Cos(Time.timeSinceLevelLoad*0.4f + 2)*0.3f + 0.7f;
 	    val = Mathf.Lerp(1, val, Multiplier);
         spriteRenderer.color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), val);
 	}
