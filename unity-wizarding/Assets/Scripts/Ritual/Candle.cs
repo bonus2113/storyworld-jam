5a1ef1317ed2cee69e6db5d991e7b583410eb4a7
Remy Stuurwold <remystuurwold+sourcetree@gmail.com>
2014-07-06 13:17:57 +0100

    candle extinguish scales down instead of disappearing

--- unity-wizarding/Assets/Scripts/Ritual/Candle.cs
+++ unity-wizarding/Assets/Scripts/Ritual/Candle.cs
@@ -6,6 +6,11 @@ public class Candle : MonoBehaviour {
     public bool b_Placed = false;
     public bool b_Extinguished = false;
     public GameObject m_CandleFlame;
+
+    private float m_TimeToExtinguish = 0.2f;
+    private float m_InternalTimer = 0.0f;
+    private Vector3 m_OrigCandleScale;
+
 	// Use this for initialization
 	void Start () {
 
@@ -14,12 +19,28 @@ public class Candle : MonoBehaviour {
             Debug.LogWarning("CandleFlame not attached.");
             Destroy(this);
         }
+        this.m_OrigCandleScale = this.m_CandleFlame.transform.localScale;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (this.m_CandleFlame != null && this.b_Extinguished)
+        {
+            this.m_InternalTimer += Time.deltaTime;
+
+            if (this.m_InternalTimer > this.m_TimeToExtinguish)
+            {
+                Destroy(this.m_CandleFlame.gameObject);
+                this.m_CandleFlame = null;
+            }
+            else
+            {
+                this.m_CandleFlame.transform.localScale = this.m_OrigCandleScale * (1.0f - this.m_InternalTimer / this.m_TimeToExtinguish);
+            }
+        }
+
 	}
 
     public Vector3 Extinguish()
@@ -30,7 +51,8 @@ public class Candle : MonoBehaviour {
         }
 
         b_Extinguished = true;
-        Destroy(this.m_CandleFlame.gameObject);
+        //Destroy(this.m_CandleFlame.gameObject);
+        //this.m_CandleFlame = null;
         return this.transform.position;
     }
 }
