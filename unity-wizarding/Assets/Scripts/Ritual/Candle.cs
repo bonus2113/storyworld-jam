73e474ab19e7bd093a4534442a1a3df55b1c9e01
Dario Seyb <dario.seyb@gmail.com>
2014-07-06 13:45:39 +0100

    -merged candle

--- unity-wizarding/Assets/Scripts/Ritual/Candle.cs
+++ unity-wizarding/Assets/Scripts/Ritual/Candle.cs
@@ -1,28 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Candle : MonoBehaviour {
+public class Candle : MonoBehaviour
+{
 
     public bool b_Placed = false;
     public bool b_Extinguished = false;
     public GameObject m_CandleFlame;
     public GameObject CandleEffect;
 
-	// Use this for initialization
-	void Start () {
+    private float m_TimeToExtinguish = 0.2f;
+    private float m_InternalTimer = 0.0f;
+    private Vector3 m_OrigCandleScale;
+
+    // Use this for initialization
+    void Start()
+    {
 
         if (this.m_CandleFlame == null)
         {
             Debug.LogWarning("CandleFlame not attached.");
             Destroy(this);
         }
+        this.m_OrigCandleScale = this.m_CandleFlame.transform.localScale;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.m_CandleFlame != null && this.b_Extinguished)
+        {
+            this.m_InternalTimer += Time.deltaTime;
+
+            if (this.m_InternalTimer > this.m_TimeToExtinguish)
+            {
+                Destroy(this.m_CandleFlame.gameObject);
+                this.m_CandleFlame = null;
+            }
+            else
+            {
+                this.m_CandleFlame.transform.localScale = this.m_OrigCandleScale * (1.0f - this.m_InternalTimer / this.m_TimeToExtinguish);
+            }
+        }
+
+    }
 
     public Vector3 Extinguish()
     {
@@ -32,8 +55,9 @@ public class Candle : MonoBehaviour {
         }
 
         b_Extinguished = true;
-        Destroy(this.m_CandleFlame.gameObject);
         GameObject.Instantiate(CandleEffect, this.m_CandleFlame.transform.position, Quaternion.identity);
+        //Destroy(this.m_CandleFlame.gameObject);
+        //this.m_CandleFlame = null;
         return this.transform.position;
     }
 }
