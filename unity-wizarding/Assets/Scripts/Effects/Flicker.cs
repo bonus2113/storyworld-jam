82beb92b2caad03e2011bcc9e206476dd48121c0
Dario Seyb <dario.seyb@gmail.com>
2014-07-06 15:08:30 +0100

    -added glowing table stuff

--- unity-wizarding/Assets/Scripts/Effects/Flicker.cs
+++ unity-wizarding/Assets/Scripts/Effects/Flicker.cs
@@ -4,18 +4,31 @@ using System.Collections;
 public class Flicker : MonoBehaviour
 {
     public float Multiplier = 1.0f;
+    public float TimeOffset = 0.0f;
+    public float TimeMultiplier = 1.0f;
 
     private SpriteRenderer spriteRenderer;
+
+    private Color baseColor;
+    private Color transparentColor;
 
 	void Start ()
 	{
 	    spriteRenderer = GetComponent<SpriteRenderer>();
+	    baseColor = spriteRenderer.color;
+	    baseColor.a = 1;
+
+	    transparentColor = baseColor;
+	    transparentColor.a = 0;
 	}
 	
 	void Update ()
 	{
-	    var val = Mathf.Sin(Time.timeSinceLevelLoad)*Mathf.Cos(Time.timeSinceLevelLoad*0.4f + 2)*0.3f + 0.7f;
+	    float time = Time.timeSinceLevelLoad + TimeOffset;
+	    time *= TimeMultiplier;
+
+        var val = Mathf.Sin(time) * Mathf.Cos(time * 0.4f + 2) * 0.3f + 0.7f;
 	    val = Mathf.Lerp(1, val, Multiplier);
-        spriteRenderer.color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), val);
+        spriteRenderer.color = Color.Lerp(baseColor, transparentColor, val);
 	}
 }
