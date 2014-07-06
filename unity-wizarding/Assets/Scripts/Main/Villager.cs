0d5ae49586539760cf8327886a04565dab0d7001
Remy Stuurwold <remystuurwold+sourcetree@gmail.com>
2014-07-06 12:59:48 +0100

    flipped villager x

--- unity-wizarding/Assets/Scripts/Main/Villager.cs
+++ unity-wizarding/Assets/Scripts/Main/Villager.cs
@@ -21,29 +21,46 @@ public class Villager : MonoBehaviour
 
     void Start()
     {
+        Vector3 tempVec = this.transform.localScale;
+        tempVec.x *= -1;
+        this.transform.localScale = tempVec;
+
         m_HeadObject = (GameObject)GameObject.Instantiate(this.m_NormalHeads[UnityEngine.Random.Range(0, this.m_NormalHeads.Count)]);
         this.m_HeadObject.transform.position = this.transform.position;
         this.m_HeadObject.transform.parent = this.transform;
         this.m_HeadObject.tag = "Head";
+        tempVec = this.m_HeadObject.transform.localScale;
+        tempVec.x *= -1;
+        this.m_HeadObject.transform.localScale = tempVec;
 
         this.m_ArmObject = (GameObject)GameObject.Instantiate(this.m_NormalArms[UnityEngine.Random.Range(0, this.m_NormalArms.Count)]);
         this.m_ArmObject.transform.position = this.transform.position;
         this.m_ArmObject.transform.parent = this.transform;
         this.m_ArmObject.tag = "Arms";
+        tempVec = this.m_ArmObject.transform.localScale;
+        tempVec.x *= -1;
+        this.m_ArmObject.transform.localScale = tempVec;
 
         this.m_BodyObject = (GameObject)GameObject.Instantiate(this.m_NormalBody[UnityEngine.Random.Range(0, this.m_NormalBody.Count)]);
         this.m_BodyObject.transform.position = this.transform.position;
         this.m_BodyObject.transform.parent = this.transform;
         this.m_BodyObject.tag = "Body";
+        tempVec = this.m_BodyObject.transform.localScale;
+        tempVec.x *= -1;
+        this.m_BodyObject.transform.localScale = tempVec;
 
         this.m_LegsObject = (GameObject)GameObject.Instantiate(this.m_NormalLegs[UnityEngine.Random.Range(0, this.m_NormalLegs.Count)]);
         this.m_LegsObject.transform.position = this.transform.position;
         this.m_LegsObject.transform.parent = this.transform;
         this.m_LegsObject.tag = "Legs";
+        tempVec = this.m_LegsObject.transform.localScale;
+        tempVec.x *= -1;
+        this.m_LegsObject.transform.localScale = tempVec;
     }
 
     public void SetIllness(Illness illness, BodyPartType bodyPart)
     {
+        GameObject switchedPart = new GameObject();
         switch (bodyPart)
         {
             case BodyPartType.Arms:
@@ -51,8 +68,7 @@ public class Villager : MonoBehaviour
                     Destroy(this.m_ArmObject.gameObject);
                     this.m_ArmObject = (GameObject)GameObject.Instantiate(illness.ArmPrefab, this.transform.position, Quaternion.identity);
                     this.m_ArmObject.tag = "Arms";
-                    this.m_ArmObject.transform.parent = this.transform;
-                    this.m_ArmObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    switchedPart = this.m_ArmObject;
                 }
                 break;
             case BodyPartType.Body:
@@ -60,8 +76,7 @@ public class Villager : MonoBehaviour
                     Destroy(this.m_BodyObject.gameObject);
                     this.m_BodyObject = (GameObject)GameObject.Instantiate(illness.BodyPrefab, this.transform.position, Quaternion.identity);
                     this.m_BodyObject.tag = "Body";
-                    this.m_BodyObject.transform.parent = this.transform;
-                    this.m_BodyObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    switchedPart = this.m_BodyObject;
                 }
                 break;
             case BodyPartType.Head:
@@ -69,8 +84,7 @@ public class Villager : MonoBehaviour
                     Destroy(this.m_HeadObject.gameObject);
                     this.m_HeadObject = (GameObject)GameObject.Instantiate(illness.HeadPrefab, this.transform.position, Quaternion.identity);
                     this.m_HeadObject.tag = "Head";
-                    this.m_HeadObject.transform.parent = this.transform;
-                    this.m_HeadObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    switchedPart = this.m_HeadObject;
                 }
                 break;
             case BodyPartType.Legs:
@@ -78,11 +92,18 @@ public class Villager : MonoBehaviour
                     Destroy(this.m_LegsObject.gameObject);
                     this.m_LegsObject = (GameObject)GameObject.Instantiate(illness.LegPrefab, this.transform.position, Quaternion.identity);
                     this.m_LegsObject.tag = "Legs";
-                    this.m_LegsObject.transform.parent = this.transform;
-                    this.m_LegsObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+                    switchedPart = this.m_LegsObject;
                 }
                 break;
         }
+
+        switchedPart.transform.parent = this.transform;
+        switchedPart.GetComponent<SpriteRenderer>().color = Color.yellow;
+
+        Vector3 tempVec = switchedPart.transform.localScale;
+        tempVec.x *= -1;
+        switchedPart.transform.localScale = tempVec;
+
     }
 
     public void StartMove(Action callback)
